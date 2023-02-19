406d8f7508ac68c629dde1f3f780bdceef9f574a
austineisele <austineisele@gmail.com>
2023-02-19 14:54:26 -0500

    Updated comments in response to checkfile test

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 public class Calculator {
         final static int umlNUMBER1 = 3;
 	@Cacheable("sum")
+	/****
+	 *Returns sum of two numbers
+	 * **/
 	public int sum(int a, int b) {
 		return a + b;
 	}
