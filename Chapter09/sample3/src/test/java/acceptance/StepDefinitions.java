9147d346743829c49a9f897106cdeb7cd17f5b9d
austineisele <austineisele@gmail.com>
2023-03-18 11:17:16 -0400

    added division to the feature and step definitions

--- Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
+++ Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
@@ -17,8 +17,8 @@ public class StepDefinitions {
     private String b;
     private String result;
 
-    @Given("^I have two numbers: (.*) and (.*)$")
-    public void i_have_two_numbers(String a, String b) throws Throwable {
+    @Given("^I have two numbers to sum: (.*) and (.*)$")
+    public void i_have_two_numbers_to_sum(String a, String b) throws Throwable {
         this.a = a;
         this.b = b;
     }
@@ -33,4 +33,23 @@ public class StepDefinitions {
     public void i_receive_as_a_result(String expectedResult) throws Throwable {
         assertEquals(expectedResult, result);
     }
+
+    @Given("^I have two numbers to divide: (.*) and (.*)$")
+    public void i_have_two_numbers_to_divide(String a, String b) throws Throwable {
+        this.a = a;
+        this.b = b;
+    }
+
+    @When("^the calculator divides them$")
+    public void the_calculator_divides_them() throws Throwable {
+        String url = String.format("%s/div?a=%s&b=%s", server, a, b);
+        result = restTemplate.getForObject(url, String.class);
+    }
+
+    @Then("^I receive (.*) as a result$")
+    public void i_receive_as_a_result(String expectedResult) throws Throwable {
+        assertEquals(expectedResult, result);
+    }
+
 }
+
--- Chapter09/sample3/src/test/resources/acceptance/calculator.feature
+++ Chapter09/sample3/src/test/resources/acceptance/calculator.feature
@@ -1,5 +1,10 @@
 Feature: Calculator
   Scenario: Sum two numbers
-    Given I have two numbers: 1 and 2
+    Given I have two numbers to sum: 1 and 2
     When the calculator sums them
+    Then I receive 3 as a result
+
+  Scenario: Divide two numbers
+    Given I have two numbers to divide: 6 and 2
+    When the calculator divides them
     Then I receive 3 as a result
